c2996f416816c4864da874cb7d20cfdb7017412e
Dennis Mckinnon <dennis.r.mckinnon@gmail.com>
2015-01-31 18:21:07 -0500

    More planar dynamics

--- organizational/planardynamics.py
+++ organizational/planardynamics.py
@@ -6,6 +6,12 @@ Fi = [0,0,0]
 def mag(vec):
 	return math.sqrt(vec[0]**2+vec[1]**2+vec[2]**2)
 
+def dif(p1, p2):
+	dv = [0,0,0]
+	for i in xrange(3):
+		dv[i] = p2[i]-p1[i]
+	return dv
+
 def Fc(Al,vec,Ac):
 	d = [0,0,0]
 	Fc = [0,0,0]
@@ -53,21 +59,98 @@ def F(vec,alv):
 			F[j] += Ft[j]
 	return F
 
-vec = map(float, sys.argv[1:4])
+def plin(alv):
+	return 1
+
+def pprf(p1, p2):
+	rf = [0,0,0]
+	Rm = 10
+	kr = 1
+	d = dif(p1, p2)
+
+	d2 = [0,0,0]
+	for i in xrange(3):
+		d2[i] = d[i]/Rm
+
+	m = mag(d2)
+
+	for i in xrange(3):
+		rf[i] = -kr*d[i]/m
+
+	return rf
+
+def drag(vel):
+	De = 1
+	Fd = [0,0,0]
+	for i in xrange(3):
+		Fd[i]=-vel[i]*De
+	return Fd
+
+def accel(pos,vel,alv):
+	FF = [0,0,0]
+	Ft = F(pos,alv)
+	for i in xrange(3):
+		FF[i]+=Ft[i]
+	Ft = drag(vel)
+	for i in xrange(3):
+		FF[i]+=Ft[i]
+	ACC = [0,0,0]
+	In = plin(alv)
+	for i in xrange(3):
+		ACC[i] = FF[i]/In
+	return ACC
+
+def findequ(pos,alv):
+	maxCycles = 100000
+	ini = pos
+	for i in xrange(maxCycles):
+		Force = F(ini,aln)
+		mForce = mag(Force)
+		for j in xrange(3):
+			ini[j] += Force[j]/2
+
+		if (mForce < 0.000001):
+			break
+	return ini
+
+pos = map(float, sys.argv[1:4])
+vel = [0,0,0]
+acc = [0,0,0]
+Force = [0,0,0]
+
 aln = map(float, sys.argv[4:11])
 
 maxCycles = 100000
-ini = vec
+tss = 0.0001
+path = []
 for i in xrange(maxCycles):
-	Force = F(ini,aln)
-	mForce = mag(Force)
+	acc = accel(pos,vel,aln)
 	for j in xrange(3):
-		ini[j] += Force[j]/2
+		vel[j]+=acc[j]*tss
+		pos[j]+=vel[j]*tss
+	path.append(pos)
+#	if (mag(vel)<0.001):
+#		print "Stopping Early"
+#		break
 
-	if (mForce < 0.000001):
-		print "Hey!"
-		break
-
-print "Final position: " + str(ini)
+print "Final Position: " + str(pos)
+print "Final Velocity: " + str(vel)
+print "Final Accelera: " + str(acc)
 
 
+f1=open("path.mat","w")
+line2= "ListPointPlot3D[{"
+flag = 0
+for r in path:
+
+	if flag==1:
+		line2 = line2 + ","
+	if flag==0:
+		flag = 1
+
+	line = ""
+	line2= line2 + "{" + str(math.floor(10000*r[0])/10000) + "," + str(math.floor(10000*r[1])/10000) + "," + str(math.floor(10000*r[2])/10000) + "}"
+
+line2 = line2 +"}]\n"
+f1.write(line2)
+f1.close()
